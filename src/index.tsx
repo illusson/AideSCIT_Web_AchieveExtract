31c990f02f50ed3df01e4f6314ca604fff06db11
SGPublic <3300633844@qq.com>
2021-03-27 20:07:12 +0800

    210327

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Extract from "./pages/Extract/Extract";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <Route path={"/"} component={ Login } />
+        <Route path={"/extract/:access_token"} component={ Extract } />
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Extract/Extract.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import History from "history"
+
+interface Props {
+    history: History
+}
+
+export default class Extract extends React.Component<Props, object>{
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
